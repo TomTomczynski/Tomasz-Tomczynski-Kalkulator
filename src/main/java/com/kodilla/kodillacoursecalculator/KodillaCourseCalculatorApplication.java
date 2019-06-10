2e0beacafd628ea07c85cf85fd656134be26fd01
TomTomczynski <tom.tomczynski@gmail.com>
2019-06-10 21:48:14 +0200

    Working

--- src/main/java/com/kodilla/kodillacoursecalculator/KodillaCourseCalculatorApplication.java
+++ src/main/java/com/kodilla/kodillacoursecalculator/KodillaCourseCalculatorApplication.java
@@ -6,27 +6,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodillaCourseCalculatorApplication {
 
-    public static class Calculator{
-        private int number=0;
+    public static class MyCalculator<overight> {
+        private int number1=0;
         private int number2=0;
-        public Calculator( ){
-
+        public MyCalculator(){
         }
-    public void setNumber(int number){
-            this.number=number;
+
+    public void setNumber1(int number){
+            this.number1=number;
     }
     public void setNumber2(int number2){
         this.number2=number2;
     }
-
+    @Override
+    public String toString(){
+            return "Number 1: "+ number1 + ", Number 2: " + number2;
+    }
     }
 
     public static void main(String[] args) {
 //        SpringApplication.run(KodillaCourseCalculatorApplication.class, args);
 
-        Calculator theCalculator = new Calculator();
-        theCalculator.setNumber(10);
+        MyCalculator theCalculator = new MyCalculator();
+        theCalculator.setNumber1(10);
         theCalculator.setNumber2(15);
+
+        System.out.println(theCalculator.toString());
 
     }
 
